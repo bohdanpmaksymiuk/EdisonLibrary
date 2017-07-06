9ab2c4bcd4ac6855e9d74a0709fd18c70c99afb3
bohdanpmaksymiuk <bohdanpmaksymiuk@gmail.com>
2017-07-06 02:34:56 +0000

    initial checkin

---
+++ go.js
@@ -0,0 +1,9 @@
+var mraa = require('mraa');
+var pin0 = new mraa.Gpio(0);
+var pin1 = new mraa.Gpio(1);
+var pin2 = new mraa.Gpio(2);
+var pin3 = new mraa.Gpio(3);
+
+pin0.dir(mraa.DIR_OUT);
+pin13.write(1);
+
